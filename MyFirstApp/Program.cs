97bcf46fcefff2781848331dddcd708dfe916ef3
lealepp <lea.lepp@tktk.ee>
2023-09-06 19:32:05 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+//rakendus küsib kasutajal kasutada oma nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter Your Name:");
+
+string UserName;
+
+UserName=Console.ReadLine();
+
+Console.WriteLine($"Hello, {UserName} !");
